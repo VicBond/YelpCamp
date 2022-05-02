6ca98039647a49c373db8164c166470c42b41b9c
Victor Bondarenko <nirasimha@gmail.com>
2022-05-02 18:43:42 +0000

    add home ejs boilerplate

--- app.js
+++ app.js
@@ -2,6 +2,35 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('Hello from new app!');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(port, () => {
   console.log(`LISTENING ON PORT ${port}`);
  })
