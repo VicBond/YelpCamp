64d0a9bd19eec5548acc6b6722ed693e458d9db3
Victor Bondarenko <nirasimha@gmail.com>
2022-05-06 12:24:26 -0400

    install and require method-override

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -22,6 +23,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
   res.render('home')
