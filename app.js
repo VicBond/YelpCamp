f8cf3d1ed61b23015b81b6c551c1abbad8583d96
Victor Bondarenko <nirasimha@gmail.com>
2022-05-13 18:18:31 -0400

    add engine ejs mate

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+const engineMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -19,6 +20,7 @@ db.once('open', () => {
 const app = express();
 const port = 3000;
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
